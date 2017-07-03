dc6ac96bb16a076b0e57c4cbc7358236794ff1c5
inner-muscle <tsuruua1223@gmail.com>
2017-07-03 18:39:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -28,6 +28,19 @@ function setCookie(c_name,value,expiredays)
   document.cookie=s;
 }
 
+var last_date = getCookie('lastDate');
+if(last_date)
+{
+  document.getElementById('cookie').textContent = '前回訪れた時間：' + last_date;
+}
+else
+{
+  document.getElementById('cookie').textContent = 'はじめまして';
+}
+
+var current_time = new Date();
+setCookie('lastDate', current_time.toString(),7);
+
 function getCookie(c_name)
 {
   var st="";
