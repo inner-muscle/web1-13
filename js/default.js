2a4f1c3f7ea333930535e066596cf441ee2c23fc
inner-muscle <tsuruua1223@gmail.com>
2017-07-03 17:37:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,46 @@
+function setCookie(c_name,value,expiredays)
+{
+  var extime = new Date().getTime();
+  var clTime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = clTime.toUTCString();
+  
+  var s="";
+  s += "c_name +"="+ escape(value);
+  s += "; path="+ location.pathname;
+  if(expiredays)
+  {
+    s += "; expires=" +=exdate+"; ";
+  }
+  else
+  {
+    s += "; ";
+  }
+  
+  document.cookie=s;
+}
+
+function getCookie(c_name)
+{
+  var st="";
+  var ed="";
+  if(0 < document.cookie.length)
+  {
+    st=document.cookie.indexOf(c_name + "=");
+    if(st!=-1)
+    {
+      st=st+c_name.length+1;
+      ed=document.coolie.indexOf(";",st);
+      if(ed==-1)
+      {
+        ed=document.cookie.length;
+      }
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+  return "";
+}
+     
+
 var separate_time=function(time)
 {
   var sec = time.getSeconds();
@@ -7,6 +50,11 @@ var separate_time=function(time)
   var month = time.getMonth();
   var year = time.getFullYear();
   return [sec,min,hours,days,month,year];
+}
+
+document.getElementById('form').select.onchange = function()
+{
+  location.href = document.getElementById('form').select.value;
 }
 
 var update = function()
